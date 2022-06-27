3cd6025a08a35fe718f1f799b95d410e6da72ab7
Daniil Romanshen <vdnv@bk.ru>
2022-06-27 21:47:00 +0600

    five task

--- hw1/5.py
+++ hw1/5.py
@@ -2,20 +2,18 @@ def count_find_num(primes, limit):
     primes_prod = 1
     for p in primes:
         primes_prod *= p
-    step = limit // primes_prod
-    count = 0
-    max_n = 0
-    for i in range(1, step + 1):
-        x = i
-        for p in primes:
-            while x % p == 0:
-                x //= p
-        if x == 1:
-            count +=1
-            max_n = i * primes_prod
-    if count == 0:
+    if primes_prod > limit:
         return []
-    return [count, max_n]
+    n = [primes_prod]
+    for i in primes:
+        o = n.copy()
+        for k in o:
+            l = i
+            while k * l <= limit:
+                n.append(k * l)
+                l = l * i
+        n.sort()
+    return [len(n), n[-1]]
     
 
 primesL = [2, 3]
@@ -37,3 +35,8 @@ assert count_find_num(primesL, limit) == [19, 960]
 primesL = [2, 3, 47]
 limit = 200
 assert count_find_num(primesL, limit) == []
+
+# timeout
+primesL = [263, 269, 337]
+limit = 563711209520283
+assert count_find_num(primesL, limit) == [15, 555749815528267]
