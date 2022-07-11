eb873a354dcd08a2779e444db99c28bf3d74267f
Daniil Romanshen <vdnv@bk.ru>
2022-07-11 17:25:44 +0600

    fix

--- hw3/1.py
+++ hw3/1.py
@@ -19,25 +19,6 @@ class CyclicIterator:
             return self.__next__()
 
 
-# cyclic_iterator = CyclicIterator(range(3))
-
-
-# for i in cyclic_iterator:
-#     print(i)
-
-class MyNumbers:
-    def __iter__(self):
-        self.a = 1
-        return self
-
-    def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
-
-
-myclass = MyNumbers()  
-myiter = iter(myclass)  
-
-for i in CyclicIterator(MyNumbers()):
-    pass
+cyclic_iterator = CyclicIterator(range(3))
+for i in cyclic_iterator:
+    print(i)
